e38bcb2f36a91908e0213cd90599f5de6b7c185a
Joaquin Alizegui <joaquinalizegui@gmail.com>
2026-03-13 12:33:19 +0100

    Arreglando mis cagadas con Prisma

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -5,9 +5,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
